437a539b6a48d043f675bf2097f0e31af4aa7d32
asb1512 <andrew@olympicmedia.com>
2021-10-27 16:16:46 -0400

    setting up Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useState, useRef } from "react";
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { useSpring, animated } from 'react-spring';
+import ReactPlayer from "react-player";
+
 
 export default function Home() {
   return (
