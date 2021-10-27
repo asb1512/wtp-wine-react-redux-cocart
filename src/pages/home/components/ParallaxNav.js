c45b57bb3ec5ad5940712e756ec1b87384e33875
asb1512 <andrew@olympicmedia.com>
2021-10-27 16:52:56 -0400

    moved file to new folder

---
+++ src/pages/home/components/ParallaxNav.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useSpring, animated } from 'react-spring'
+import UpArrowButton from './UpArrowButton'
+import DownArrowButton from './DownArrowButton'
+
+import './ParallaxNav.css'
+
+export default function ParallaxNav(props, ref) {
+
+  const animatedStyle = useSpring({
+    opacity: props.toggle ? 1 : 0,
+    delay: 1000
+  })
+
+  const focusStyle = "-focused"
+
+  return (
+    <animated.div
+      className="parallax-nav"
+      style={animatedStyle}
+    >
+      <div
+        className={`parallax-nav-item${props.focus === 'cab' ? focusStyle : ''}`}
+      >
+        – CABERNET SAUVIGNON
+      </div>
+
+      <div
+        className={`parallax-nav-item${props.focus === 'chard' ? focusStyle : ''}`}
+      >
+        – CHARDONNAY
+      </div>
+      <div
+        className={`parallax-nav-item${props.focus === 'video' ? focusStyle : ''}`}
+      >
+        – MEET THE BRAND
+      </div>
+    </animated.div>
+  )
+}
