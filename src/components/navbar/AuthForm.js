dd92e77fd9f411855be3e1eb68de66a8520190a0
asb1512 <andrew@olympicmedia.com>
2021-10-28 13:30:45 -0400

    setting up dispatch with Redux

--- src/components/navbar/AuthForm.js
+++ src/components/navbar/AuthForm.js
@@ -1,27 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { authenticateUser } from '../../actions/authenticateUser';
 import { useSpring, animated } from 'react-spring';
 
 import './AuthForm.css';
 
-export default function AuthForm(props) {
+function AuthForm(props) {
+
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('')
+
+  const onEmailChange = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const onPwChange = (event) => {
+    setPw(event.target.value)
+  }
 
   const authFormStyle = useSpring({
     opacity: props.toggle ? 1 : 0
   })
 
+  const handleAuthFormSubmit = (event) => {
+    event.preventDefault();
+    props.authenticateUser();
+  }
+
   return (
     <animated.div className="auth-container" style={authFormStyle}>
       <h3>LOGIN/REGISTER</h3>
 
-      <form className="auth-form">
-        <label>EMAIL</label>
-        <input type="text" />
+      <form className="auth-form" onSubmit={handleAuthFormSubmit}>
 
-        <label>PASSWORD</label>
-        <input type="password" />
+        <label className="auth-label">EMAIL</label>
+        <input 
+          type="text" 
+          value={email} 
+          className="auth-field"
+          placeholder="Email"
+          onChange={onEmailChange} 
+        />
 
-        <input type="submit" value="SUBMIT"/>
+        <label className="auth-label">PASSWORD</label>
+        <input 
+          type="password" 
+          value={pw} 
+          className="auth-field"
+          placeholder="Password"
+          onChange={onPwChange} 
+        />
+
+        <input type="submit" value="SUBMIT" className="auth-button" />
+        
       </form>
     </animated.div>
   )
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    authenticateUser: () => dispatch(authenticateUser())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AuthForm);
