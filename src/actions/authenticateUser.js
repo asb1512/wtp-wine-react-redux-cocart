da97a32d7d52952ba15f52471c195ca54c8488d6
asb1512 <andrew@olympicmedia.com>
2021-10-29 10:47:49 -0400

    added console logs for debugging

--- src/actions/authenticateUser.js
+++ src/actions/authenticateUser.js
@@ -1,19 +1,31 @@
 import axios from 'axios';
-// import CoCartAPI from '@cocart/cocart-rest-api';
 
-export function authenticateUser() {
+
+export function authenticateUser(email, pw) {
   return function (dispatch) {
-    dispatch({type: "AUTHENTICATE_USER_LOADING"})
+    dispatch({ type: "AUTHENTICATE_USER_LOADING" })
     console.log("Base URL:", process.env.REACT_APP_BASE_URL)
 
     const loginData = {
-      username: 'andrew',
-      password: 'Thisisgoingtobefun1!',
+      username: email,
+      password: pw,
     }
+
+    console.log("User Email:", email)
+    console.log("User Password", pw)
+    console.log("loginData Obj:", loginData)
+    debugger
 
     return axios.post(`${process.env.REACT_APP_BASE_URL}/wp-json/jwt-auth/v1/token`, loginData)
 
-      .then(resp => console.log(resp))
-      .catch(error => console.log(error))
+      .then(resp => {
+        console.log("full response:", resp)
+        console.log("Returned token:", resp.data.token)
+        // dispatch({ type: "AUTHENTICATE_USER", resp })
+      })
+      .catch(error => {
+        console.log("Error message:", error.message)
+        dispatch({ type: "AUTHENTICATION_ERROR", error })
+      })
   }
 }
