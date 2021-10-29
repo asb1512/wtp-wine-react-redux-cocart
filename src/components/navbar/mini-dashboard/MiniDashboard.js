c79305aabb6e7a7ebcfda7c2574f3b02e526913d
asb1512 <andrew@olympicmedia.com>
2021-10-29 17:02:15 -0400

    created menu options

---
+++ src/components/navbar/mini-dashboard/MiniDashboard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './MiniDashboard.css';
+
+export default function MiniDashboard() {
+
+  return (
+    <div className="mini-dashboard-container">
+
+      <div className="mini-dashboard-div">
+        ORDERS
+      </div>
+
+      <div className="mini-dashboard-div">
+        SUBSCRIPTIONS
+      </div>
+
+      <div className="mini-dashboard-div">
+        ADDRESSES
+      </div>
+
+      <div className="mini-dashboard-div">
+        ACCOUNT DETAILS
+      </div>
+
+      <div className="mini-dashboard-div">
+        LOGOUT
+      </div>
+
+    </div>
+  )
+}
