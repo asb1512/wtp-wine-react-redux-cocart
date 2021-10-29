e008c0f0c18ee6dad227c0bb4e742df8e16356a6
asb1512 <andrew@olympicmedia.com>
2021-10-29 12:57:34 -0400

    removed console logs and uncommented dispatch call

--- src/actions/authenticateUser.js
+++ src/actions/authenticateUser.js
@@ -1,27 +1,22 @@
 import axios from 'axios';
 
 
-export function authenticateUser(email, pw) {
+export function authenticateUser(ui, pw) {
   return function (dispatch) {
     dispatch({ type: "AUTHENTICATE_USER_LOADING" })
     console.log("Base URL:", process.env.REACT_APP_BASE_URL)
 
     const loginData = {
-      username: email,
+      username: ui,
       password: pw,
     }
-
-    console.log("User Email:", email)
-    console.log("User Password", pw)
-    console.log("loginData Obj:", loginData)
-    debugger
 
     return axios.post(`${process.env.REACT_APP_BASE_URL}/wp-json/jwt-auth/v1/token`, loginData)
 
       .then(resp => {
         console.log("full response:", resp)
         console.log("Returned token:", resp.data.token)
-        // dispatch({ type: "AUTHENTICATE_USER", resp })
+        dispatch({ type: "AUTHENTICATE_USER", resp })
       })
       .catch(error => {
         console.log("Error message:", error.message)
