10a7aba6b30607f736cda3627523a1f06c7cb7ff
asb1512 <andrew@olympicmedia.com>
2021-10-29 14:50:12 -0400

    added options to MiniDashboard

--- src/components/navbar/MiniDashboard.js
+++ src/components/navbar/MiniDashboard.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
 
-export default function MiniDashboard() {
+import './MiniDashboard.css';
+
+export default function MiniDashboard(props) {
+
+  const miniDashboardStyle = useSpring({
+    opacity: props.toggle ? 1 : 0
+  })
+
   return (
-    <div>
-      
-    </div>
+    <animated.div className="mini-dashboard" style={miniDashboardStyle}>
+
+      <div className="mini-dashboard-container">
+
+        <div className="mini-dashboard-item">
+          ORDERS
+        </div>
+
+        <div className="mini-dashboard-item">
+          SUBSCRIPTIONS
+        </div>
+
+        <div className="mini-dashboard-item">
+          ADDRESSES
+        </div>
+
+        <div className="mini-dashboard-item">
+          ACCOUNT DETAILS
+        </div>
+
+        <div className="mini-dashboard-item">
+          LOGOUT
+        </div>
+
+      </div>
+
+    </animated.div>
   )
 }
