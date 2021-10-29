a8800b7a7324e8f35c8a56abfe70bdeb8837f972
asb1512 <andrew@olympicmedia.com>
2021-10-29 10:47:28 -0400

    added authentication error case

--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,15 +1,32 @@
-const defaultState = {};
+const defaultState = {
+  userLoggedIn: false,
+};
 
 export default function mainReducer(state = defaultState, action) {
   switch (action.type) {
     case "AUTHENTICATE_USER":
       return {
         ...state,
-        currentUser: action.payload
+        userLoggedIn: true,
+        currentUser: {
+          token: action.data.token,
+          username: action.data.user_display_name,
+          email: action.data.user_email,
+        },
+        loading: false,
       }
 
     case "AUTHENTICATE_USER_LOADING":
       return { ...state, loading: true }
+
+    case "AUTHENTICATION_ERROR":
+      return {
+        ...state,
+        currentError: {
+          errorMessage: action.error.message,
+        },
+        loading: false,
+      }
 
     default: return state
   }
