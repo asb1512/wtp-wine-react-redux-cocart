08cd8a304e0f4d8718598ea9f7eb8a7cfa153b34
asb1512 <andrew@olympicmedia.com>
2021-10-29 16:59:31 -0400

    removed unnecessary console logs

--- src/actions/authenticateUser.js
+++ src/actions/authenticateUser.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 export function authenticateUser(ui, pw) {
   return function (dispatch) {
     dispatch({ type: "AUTHENTICATE_USER_LOADING" })
-    console.log("Base URL:", process.env.REACT_APP_BASE_URL)
 
     const loginData = {
       username: ui,
@@ -14,8 +13,6 @@ export function authenticateUser(ui, pw) {
     return axios.post(`${process.env.REACT_APP_BASE_URL}/wp-json/jwt-auth/v1/token`, loginData)
 
       .then(resp => {
-        console.log("full response:", resp)
-        console.log("Returned token:", resp.data.token)
         dispatch({ type: "AUTHENTICATE_USER", resp })
       })
       .catch(error => {
