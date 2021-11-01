df349004edf792be557f1a2bc654c87a4d562c95
asb1512 <andrew@olympicmedia.com>
2021-11-01 16:52:10 -0400

    linking each option and routing to Dashboard with nested routes

--- src/components/navbar/mini-dashboard/MiniDashboard.js
+++ src/components/navbar/mini-dashboard/MiniDashboard.js
@@ -1,59 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './MiniDashboard.css';
 
-import UserOrders from './UserOrders';
-
 export default function MiniDashboard() {
-
-  const [showOrders, setOrders] = useState(false)
-  const handleOrdersClick = () => {
-    console.log("handleOrdersClick:", showOrders)
-    return <UserOrders toggle={showOrders} />
-  }
-
-  const [showSubscriptions, setSubscriptions] = useState(false)
-  const handleSubscriptionsClick = () => {
-    
-  }
 
   return (
     <div className="mini-dashboard-container">
 
-      <div
-        className="mini-dashboard-div"
-        onClick={handleOrdersClick}
-      >
-        <span className="mini-dashboard-option">ORDERS</span>
-      </div>
+      <Link to="/dashboard/orders" className="mini-dashboard-link">
+        <div className="mini-dashboard-div">ORDERS</div>
+      </Link>
 
-      <div
-        className="mini-dashboard-div"
-        
-      >
-        <span className="mini-dashboard-option">SUBSCRIPTIONS</span>
-      </div>
+      <Link to="/dashboard/subscriptions" className="mini-dashboard-link">
+        <div className="mini-dashboard-div">SUBSCRIPTIONS</div>
+      </Link>
 
-      <div
-        className="mini-dashboard-div"
-        
-      >
-        <span className="mini-dashboard-option">ADDRESSES</span>
-      </div>
+      <Link to="/dashboard/addresses" className="mini-dashboard-link">
+        <div className="mini-dashboard-div">ADDRESSES</div>
+      </Link>
 
-      <div
-        className="mini-dashboard-div"
-        
-      >
-        <span className="mini-dashboard-option">ACCOUNT DETAILS</span>
-      </div>
+      <Link to="/dashboard/account" className="mini-dashboard-link">
+        <div className="mini-dashboard-div">ACCOUNT</div>
+      </Link>
 
-      <div
-        className="mini-dashboard-div"
-        
-      >
-        <span className="mini-dashboard-option">LOGOUT</span>
-      </div>
+      <div className="mini-dashboard-div">LOGOUT</div>
 
     </div>
   )
