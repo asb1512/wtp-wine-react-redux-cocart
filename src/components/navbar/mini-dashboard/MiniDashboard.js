ab0593644b88ff2f22c28654078bcdb840ed3d86
asb1512 <andrew@olympicmedia.com>
2021-11-01 11:52:59 -0400

    building out MiniDashboard

--- src/components/navbar/mini-dashboard/MiniDashboard.js
+++ src/components/navbar/mini-dashboard/MiniDashboard.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './MiniDashboard.css';
 
+import UserOrders from './UserOrders';
+
 export default function MiniDashboard() {
+
+  const [showOrders, setOrders] = useState(false)
+  const handleOrdersClick = () => {
+    console.log("handleOrdersClick:", showOrders)
+    return <UserOrders toggle={showOrders} />
+  }
+
+  const [showSubscriptions, setSubscriptions] = useState(false)
+  const handleSubscriptionsClick = () => {
+    
+  }
 
   return (
     <div className="mini-dashboard-container">
 
-      <div className="mini-dashboard-div">
-        ORDERS
+      <div
+        className="mini-dashboard-div"
+        onClick={handleOrdersClick}
+      >
+        <span className="mini-dashboard-option">ORDERS</span>
       </div>
 
-      <div className="mini-dashboard-div">
-        SUBSCRIPTIONS
+      <div
+        className="mini-dashboard-div"
+        
+      >
+        <span className="mini-dashboard-option">SUBSCRIPTIONS</span>
       </div>
 
-      <div className="mini-dashboard-div">
-        ADDRESSES
+      <div
+        className="mini-dashboard-div"
+        
+      >
+        <span className="mini-dashboard-option">ADDRESSES</span>
       </div>
 
-      <div className="mini-dashboard-div">
-        ACCOUNT DETAILS
+      <div
+        className="mini-dashboard-div"
+        
+      >
+        <span className="mini-dashboard-option">ACCOUNT DETAILS</span>
       </div>
 
-      <div className="mini-dashboard-div">
-        LOGOUT
+      <div
+        className="mini-dashboard-div"
+        
+      >
+        <span className="mini-dashboard-option">LOGOUT</span>
       </div>
 
     </div>
