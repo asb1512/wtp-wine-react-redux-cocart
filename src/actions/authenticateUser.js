6efbd786b6520609937fb15ae65030ea4058d7b4
asb1512 <andrew@olympicmedia.com>
2021-11-01 11:53:35 -0400

    added second API call to request the customer's ID

--- src/actions/authenticateUser.js
+++ src/actions/authenticateUser.js
@@ -9,11 +9,20 @@ export function authenticateUser(ui, pw) {
       username: ui,
       password: pw,
     }
-
+    // returns a token from WC and user info; unfortunately doesn't include customer id
     return axios.post(`${process.env.REACT_APP_BASE_URL}/wp-json/jwt-auth/v1/token`, loginData)
 
       .then(resp => {
         dispatch({ type: "AUTHENTICATE_USER", resp })
+        // this will retrieve customer_id
+        axios.get(`${process.env.REACT_APP_BASE_URL}/wp-json/wc/v3/customers?email=${resp.data.user_email}&consumer_key=${process.env.REACT_APP_CONSUMER_KEY}&consumer_secret=${process.env.REACT_APP_CONSUMER_SECRET}`)
+
+          .then(resp => {
+            console.log("Customer ID retrieval:", resp)
+          })
+          .catch(error => {
+            console.log("Customer ID retrieval error:", error)
+          })
       })
       .catch(error => {
         console.log("Error message:", error.message)
