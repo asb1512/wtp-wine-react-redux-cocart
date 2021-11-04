2007005a24e120ea25a6c326844bbd713d57ad71
asb1512 <andrew@olympicmedia.com>
2021-11-04 12:06:43 -0400

    added onClick passed from Nav to close menu on click

--- src/components/navbar/mini-dashboard/MiniDashboard.js
+++ src/components/navbar/mini-dashboard/MiniDashboard.js
@@ -3,30 +3,50 @@ import { Link } from 'react-router-dom';
 
 import './MiniDashboard.css';
 
-export default function MiniDashboard() {
+export default function MiniDashboard(props) {
 
   const [logUserOut, setLogOut] = useState(false)
 
   return (
     <div className="mini-dashboard-container">
 
-      <Link to="/dashboard/orders" className="mini-dashboard-link">
+      <Link 
+        to="/dashboard/orders" 
+        className="mini-dashboard-link"
+        onClick={() => props.setAuthOpen(false)}
+      >
         <div className="mini-dashboard-div">ORDERS</div>
       </Link>
 
-      <Link to="/dashboard/subscriptions" className="mini-dashboard-link">
+      <Link 
+        to="/dashboard/subscriptions" 
+        className="mini-dashboard-link"
+        onClick={() => props.setAuthOpen(false)}
+      >
         <div className="mini-dashboard-div">SUBSCRIPTIONS</div>
       </Link>
 
-      <Link to="/dashboard/addresses" className="mini-dashboard-link">
+      <Link 
+        to="/dashboard/addresses" 
+        className="mini-dashboard-link"
+        onClick={() => props.setAuthOpen(false)}
+      >
         <div className="mini-dashboard-div">ADDRESSES</div>
       </Link>
 
-      <Link to="/dashboard/account" className="mini-dashboard-link">
+      <Link 
+        to="/dashboard/account" 
+        className="mini-dashboard-link"
+        onClick={() => props.setAuthOpen(false)}
+      >
         <div className="mini-dashboard-div">ACCOUNT</div>
       </Link>
 
-      <Link to="#" className="mini-dashboard-link" onClick={setLogOut(true)}>
+      <Link 
+        to="#" 
+        className="mini-dashboard-link" 
+        onClick={() => setLogOut(true)}
+      >
         <div className="mini-dashboard-div">LOGOUT</div>
       </Link>
 
