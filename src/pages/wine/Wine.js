d9079b84715d55fc528b513b3828a1702db00eee
asb1512 <andrew@olympicmedia.com>
2021-11-05 14:36:52 -0400

    added method that makes the non-selected bottle disappear on click

--- src/pages/wine/Wine.js
+++ src/pages/wine/Wine.js
@@ -14,10 +14,19 @@ export default function Wine(props) {
   const [showCabBg, setCabBg] = useState(false);
   const cabBgStyle = useSpring({
     opacity: showCabBg ? 1 : 0,
+    config: { duration: 300 }
   })
   const [showCabTitle, setCabTitle] = useState(false);
   const cabTitleStyle = useSpring({
-    transform: showCabTitle ? 'translate(0px,0px)' : 'translate(700px,0px)'
+    transform: showCabTitle ? 'translate(0px,0px)' : 'translate(900px,0px)'
+  })
+  const [showCabDesc, setCabDesc] = useState(false)
+  const cabDescStyle = useSpring({
+    transform: showCabDesc ? 'translate(0px,0px)' : 'translate(-900px,0px)'
+  })
+  const [hideChard, setHideChard] = useState(false)
+  const cabClickStyle = useSpring({
+    opacity: hideChard ? 0 : 1
   })
   const handleCabMouseEnter = () => {
     setCabBg(true)
@@ -27,16 +36,23 @@ export default function Wine(props) {
     setCabBg(false)
     setCabTitle(false)
   }
+  const handleCabClick = () => {
+    setCabDesc(true)
+    setHideChard(true)
+    setCabTitle(true)
+    setCabBg(true)
+  }
 
 
 
   const [showChardBg, setChardBg] = useState(false);
   const chardBgStyle = useSpring({
     opacity: showChardBg ? 1 : 0,
+    config: { duration: 300 }
   })
   const [showChardTitle, setChardTitle] = useState(false);
   const chardTitleStyle = useSpring({
-    transform: showChardTitle ? 'translate(0px,0px)' : 'translate(-700px,0px)',
+    transform: showChardTitle ? 'translate(0px,0px)' : 'translate(-900px,0px)',
   })
   const handleChardMouseEnter = () => {
     setChardBg(true)
@@ -51,44 +67,62 @@ export default function Wine(props) {
 
   return (
 
-    <div className="wine-container">
+    <div className="wine-cntr">
 
       <div className="wine-options">
+
+
         <img
           src={cab}
           alt="We The People 2018 Cabernet Sauvignon"
           className="wtp-cab"
           onMouseEnter={() => handleCabMouseEnter()}
           onMouseLeave={() => handleCabMouseLeave()}
+          onClick={() => handleCabClick()}
         />
-        <div className="cab-title-container">
-          <animated.div 
+        <div className="cab-title-cntr">
+          <animated.div
             className="cab-title"
             style={cabTitleStyle}
           >
             2018 CALIFORNIA CABERNET SAUVIGNON
+            <div className="wine-subheading">BLUEBERRY | CHERRY | TANNIN | VELVET</div>
+          </animated.div>
+        </div>
+        <div className="cab-desc-anchor">
+          <animated.div className="cab-desc-cntr" style={cabDescStyle}>
+            <div className="cab-desc">
+              $29.99
+            </div>
+            <button className="addtocart-btn">ADD TO CART</button>
           </animated.div>
         </div>
 
-        <img
+
+        <animated.img
           src={chard}
           alt="We The People 2019 Chardonnay"
           className="wtp-chard"
+          style={cabClickStyle}
           onMouseEnter={() => handleChardMouseEnter()}
           onMouseLeave={() => handleChardMouseLeave()}
         />
-        <div className="chard-title-container">
-          <animated.div 
+        <div className="chard-title-cntr">
+          <animated.div
             className="chard-title"
             style={chardTitleStyle}
-          >2019 CALIFORNIA CHARDONNAY</animated.div>
+          >
+            2019 CALIFORNIA CHARDONNAY
+            <div className="wine-subheading">STONE FRUIT | BAKED APPLES | FRESH ACIDITY</div>
+          </animated.div>
         </div>
+
 
       </div>
 
       <animated.div className="cab-bg" style={cabBgStyle} />
       <animated.div className="chard-bg" style={chardBgStyle} />
-      
+
     </div>
   )
 }
