e7e32e80d21dd98e0e883340416bb82494c8d964
asb1512 <andrew@olympicmedia.com>
2021-11-08 20:21:44 -0500

    updated MiniCart to get state from store, delete items from the cart, and render based on whether the cart is empty or not

--- src/components/navbar/MiniCart.js
+++ src/components/navbar/MiniCart.js
@@ -1,25 +1,94 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSpring, animated } from 'react-spring';
+import { connect } from 'react-redux';
+import { removeItemFromCart } from '../../actions/userCart';
+
+import cab from '../../images/wine/cabernet.png';
+import chard from '../../images/wine/chardonnay.png';
 
 import './MiniCart.css';
 
-export default function MiniCart(props) {
+function MiniCart(props) {
 
   const cartStyle = useSpring({
-    right: props.toggle ? '0%' : '-30%'
+    right: props.toggle ? '0%' : '-47%'
   })
 
-  const [subTotal, setSubTotal] = useState(0)
+  const addDecimalToSubtotal = () => {
+    if (props.cartItems && props.cartItems.length > 0) {
+      let splitPrice = props.cartTotals.subtotal.split('')
+      splitPrice.splice(-2, 0, '.')
+      let formattedPrice = splitPrice.join('')
+      return formattedPrice;
+    } else return '0.00';
+  }
+
+  const handleItemDeletion = (itemKey) => {
+    props.removeItemFromCart(itemKey, props.cartKey)
+  }
+
+  const renderMiniCartContent = () => {
+    if (props.cartItems && props.cartItems.length > 0) {
+      return (
+        <>
+          {props.cartItems?.map(item => {
+            return (
+              <div className="minicart-item" key={item.id}>
+                <img
+                  src={item.id === 13 ? cab : chard}
+                  alt={`We The People Wine ${item.name}`}
+                  className="minicart-item-img"
+                />
+                <div className="minicart-item-name">{item.name}</div>
+                <div className="minicart-item-quantity">{item.quantity.value}</div>
+                <div className="minicart-item-price">${item.price}</div>
+                <div
+                  className="minicart-item-remove"
+                  onClick={() => handleItemDeletion(item.item_key)}
+                >
+                  x
+                </div>
+              </div>
+            )
+          })}
+        </>
+      )
+    } else {
+      return (
+        <>
+          <div className="minicart-no-items">There are no items in your cart.</div>
+        </>
+      )
+    }
+  }
 
   return (
     <animated.div className="minicart-container" style={cartStyle}>
       <div className="minicart-title">YOUR CART</div>
 
       <div className="minicart-items">
-
+        {renderMiniCartContent()}
       </div>
 
-      <div className="minicart-total">SUBTOTAL : ${subTotal}</div>
+      <div className="minicart-total">SUBTOTAL : ${addDecimalToSubtotal()}</div>
     </animated.div>
   )
 }
+
+const mapStateToProps = (state) => {
+  if (state.userCart) {
+    return {
+      cartItems: state.userCart.items ? state.userCart.items : [],
+      cartTotals: state.userCart.totals ? state.userCart.totals : null,
+      cartKey: state.userCart.cart_key ? state.userCart.cart_key : null,
+    }
+  } else return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    removeItemFromCart: (itemKey, cartKey) => dispatch(removeItemFromCart(itemKey, cartKey)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MiniCart);
