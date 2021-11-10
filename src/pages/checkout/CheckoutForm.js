e490a29df53b7db09bae3f88eb858eb15c0d6248
asb1512 <andrew@olympicmedia.com>
2021-11-10 10:58:24 -0500

    created Checkout Form to handle Stripe payment elements

---
+++ src/pages/checkout/CheckoutForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { PaymentElement } from '@stripe/react-stripe-js';
+
+export default function CheckoutForm() {
+  return (
+    <form>
+      
+    </form>
+  )
+}
