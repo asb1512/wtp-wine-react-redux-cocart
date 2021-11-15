7e73d0942bd3ed668f6085b6de830892b93f62b9
asb1512 <andrew@olympicmedia.com>
2021-11-15 14:11:17 -0500

    created OrderSuccess as a redirect page after a successful Stripe payment

---
+++ src/pages/checkout/OrderSuccess.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './OrderSuccess.css';
+
+export default function OrderSuccess() {
+  return (
+    <div className="order-success-container">
+      
+    </div>
+  )
+}
