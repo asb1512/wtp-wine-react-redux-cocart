7707b8fd055133075a51c7af62599b6e32f5cee7
asb1512 <andrew@olympicmedia.com>
2021-11-15 14:10:55 -0500

    added conditional rendering that will only show Stripe PaymentElement if user has a current and valid order, as well as a payment intent ID

--- src/pages/checkout/CheckoutSummary.js
+++ src/pages/checkout/CheckoutSummary.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import './Checkout.css';
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
+import { connect } from 'react-redux';
 import CheckoutForm from './CheckoutForm';
 
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLIC_KEY}`)
 
-export default function CheckoutSummary() {
+function CheckoutSummary(props) {
 
-  // const renderLoading = () => {
-  //   return (
+  const renderContent = () => {
+    if (props.stripe && props.orderCheckout) {
+      const options = { clientSecret: props.stripe.stripeClientSecret }
+      return (
+        <Elements stripe={stripePromise} options={options}>
+          <CheckoutForm />
+        </Elements>
+      )
 
-  //   )
-  // }
+    } else {
+      return (
+        <div className="invalid-checkout">
+          You don't have a pending order. Please add items to your cart.
+        </div>
+      )
+    }
+  }
 
   return (
     <div className="checkout-container">
-      
+      {renderContent()}
     </div>
   )
 }
+
+const mapStateToProps = (state) => {
+  if (state.currentUser.stripe && state.currentUser.orderCheckout) {
+    return {
+      stripe: state.currentUser.stripe,
+      orderCheckout: state.currentUser.orderCheckout,
+    }
+  } else return {}
+}
+
+export default connect(mapStateToProps)(CheckoutSummary);
