27a8ef1cbb3dd5d37d6ab9fcf7ca44b968a24fef
asb1512 <andrew@olympicmedia.com>
2021-11-15 14:08:23 -0500

    building out handleSubmit function for Stripe payments

--- src/pages/checkout/CheckoutForm.js
+++ src/pages/checkout/CheckoutForm.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { PaymentElement } from '@stripe/react-stripe-js';
+import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
 
 export default function CheckoutForm() {
+
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const [btnDisabled, setBtnDisable] = useState(true)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+
+    if (stripe && elements) {
+      setBtnDisable(false)
+      return;
+    }
+
+    const result = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `http://localhost:3000/order-success`,
+      },
+    })
+  }
+
   return (
-    <form>
-      
+    <form className="stripe-checkout">
+
+      <PaymentElement />
+      <button 
+        className="stripe-submit-btn"
+        disabled={btnDisabled}
+      >
+        SUBMIT
+      </button>
+
     </form>
   )
 }
