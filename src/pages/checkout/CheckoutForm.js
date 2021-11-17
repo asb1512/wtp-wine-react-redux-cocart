43d2d28293b2795bf12be225276699d86d721d2f
asb1512 <andrew@olympicmedia.com>
2021-11-17 11:45:19 -0500

    not final: still building out Stripe payment integration

--- src/pages/checkout/CheckoutForm.js
+++ src/pages/checkout/CheckoutForm.js
@@ -1,41 +1,114 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useStripe, useElements, CardElement } from '@stripe/react-stripe-js';
 
-import { useStripe, useElements, PaymentElement } from '@stripe/react-stripe-js';
-
-export default function CheckoutForm() {
+function CheckoutForm(props) {
 
   const stripe = useStripe();
   const elements = useElements();
 
-  const [btnDisabled, setBtnDisable] = useState(true)
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [btnDisabled, setBtnDisable] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault()
-
-    if (stripe && elements) {
-      setBtnDisable(false)
+  useEffect(() => {
+    if (!stripe || props.stripe.stripeClientSecret) {
       return;
     }
 
-    const result = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        return_url: `http://localhost:3000/order-success`,
+    stripe.retrievePaymentIntent(props.stripe.stripeClientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe, props.stripe.stripeClientSecret]);
+
+
+  const handleSubmit = async (event) => {
+    console.log("handleSubmit was called!")
+    event.preventDefault()
+
+    const billingDetails = {
+      name: props.orderCheckout.billing.first_name + " " + props.orderCheckout.billing.last_name,
+      email: props.orderCheckout.billing.email,
+      address: {
+        city: props.orderCheckout.billing.city,
+        line1: props.orderCheckout.billing.address_1,
+        state: props.orderCheckout.billing.state,
+        postal_code: props.orderCheckout.billing.postcode,
       },
-    })
+    }
+
+    if (!stripe || !elements) {
+      setBtnDisable(true)
+      return;
+    }
+
+    // if (!error) {
+
+    // }
+
+    const result = await stripe.confirmPayment(
+      props.stripe.stripeClientSecret, 
+      {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: billingDetails
+        },
+        confirmParams: {
+          return_url: `http://localhost:3000/order-success`,
+        },
+      }
+    )
+
+    if (result.error) {
+      renderStripeError(result.error.message)
+      console.log("Stripe Error Message:", result.error.message)
+    }
+  }
+
+  const renderStripeError = (errorMsg) => {
+    return (
+      <div className="stripe-error">
+        {errorMsg}
+      </div>
+    )
   }
 
   return (
-    <form className="stripe-checkout">
+    <form className="stripe-checkout" onSubmit={handleSubmit}>
 
-      <PaymentElement />
+      <CardElement />
       <button 
+        type="submit"
         className="stripe-submit-btn"
-        disabled={btnDisabled}
+        disabled={!stripe}
       >
-        SUBMIT
+        PAY {`$${props.orderCheckout.total}`}
       </button>
 
     </form>
   )
 }
+
+const mapStateToProps = state => {
+  if (state.currentUser.orderCheckout) {
+    return {
+      orderCheckout: state.currentUser.orderCheckout,
+      stripe: state.currentUser.stripe,
+    }
+  } else return {}
+}
+
+export default connect(mapStateToProps)(CheckoutForm);
