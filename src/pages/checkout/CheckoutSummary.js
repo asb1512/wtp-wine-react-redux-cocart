851652587271dee3425590b4fa8641e1d39c86e7
asb1512 <andrew@olympicmedia.com>
2021-11-17 11:44:17 -0500

    commented out unused code and updated format of state from Redux store

--- src/pages/checkout/CheckoutSummary.js
+++ src/pages/checkout/CheckoutSummary.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Checkout.css';
 import { connect } from 'react-redux';
+// import { useLocation } from 'react-router-dom';
 import CheckoutForm from './CheckoutForm';
+// import Loader from '../../components/Loader';
 
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
@@ -9,9 +11,29 @@ const stripePromise = loadStripe(`${process.env.REACT_APP_STRIPE_PUBLIC_KEY}`)
 
 function CheckoutSummary(props) {
 
+  // need to find a way to verify that the user has been redirected from /order-summary vs. a user going straight to /checkout
+  // const search = useLocation().search;
+  // const cartHash = new URLSearchParams(search).get('cart');
+  // console.log("URL Query Params:", cartHash)
+
   const renderContent = () => {
     if (props.stripe && props.orderCheckout) {
-      const options = { clientSecret: props.stripe.stripeClientSecret }
+
+      const appearance = {
+        style: {
+          base: {
+            color: '#fff',
+          },
+          invalid: {},
+          complete: {},
+        },
+      }
+
+      const options = { 
+        client_secret: props.stripe.stripeClientSecret,
+        appearance: appearance,
+      }
+
       return (
         <Elements stripe={stripePromise} options={options}>
           <CheckoutForm />
@@ -21,7 +43,7 @@ function CheckoutSummary(props) {
     } else {
       return (
         <div className="invalid-checkout">
-          You don't have a pending order. Please add items to your cart.
+          Your order hasn't been verified by our systems. <br /> Please checkout by opening your cart and clicking 'Checkout'.
         </div>
       )
     }
@@ -35,10 +57,12 @@ function CheckoutSummary(props) {
 }
 
 const mapStateToProps = (state) => {
-  if (state.currentUser.stripe && state.currentUser.orderCheckout) {
+  if (state.currentUser.stripe && state.currentUser.orderCheckout && state.userCart) {
     return {
       stripe: state.currentUser.stripe,
       orderCheckout: state.currentUser.orderCheckout,
+      userCart: state.userCart,
+      checkoutLoading: state.checkoutLoading,
     }
   } else return {}
 }
