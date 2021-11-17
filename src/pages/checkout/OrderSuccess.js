b22e4e485d925f5ef870bb78a7660cf4a965f4f9
asb1512 <andrew@olympicmedia.com>
2021-11-17 11:42:36 -0500

    changed message displayed to user upon a successful Stripe payment

--- src/pages/checkout/OrderSuccess.js
+++ src/pages/checkout/OrderSuccess.js
@@ -4,7 +4,9 @@ import './OrderSuccess.css';
 export default function OrderSuccess() {
   return (
     <div className="order-success-container">
-      
+      <div className="order-success-msg">
+        Thanks for your order! Your order is being processed and you will receive an email once your order has shipped.
+      </div>
     </div>
   )
 }
