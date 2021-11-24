9e8df9f94ee8a2fbea6eca79df193b591df599fb
asb1512 <andrew@olympicmedia.com>
2021-11-24 15:23:24 -0500

    added line break

--- src/pages/home/components/CabDesc.js
+++ src/pages/home/components/CabDesc.js
@@ -16,7 +16,7 @@ export default function CabDesc(props) {
       <animated.h1
         style={animatedStyle}
       >
-        CABERNET SAUVIGNON
+        CABERNET <br /> SAUVIGNON
       </animated.h1>
       <div>
         Our <span className="wine-desc-span-cab">2018 California Cabernet Sauvignon</span> boasts a deep ruby and purple hue with flavors of blueberry and cherry. Soft tannins round the full-bodied mouthfeel with a velvet finish.
