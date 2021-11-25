7aea144eaec3df117c7caf2515055dd4b4824de0
asb1512 <andrew@olympicmedia.com>
2021-11-25 01:41:25 -0500

    building out animations on button clicks

--- src/pages/wine/WineMobile.js
+++ src/pages/wine/WineMobile.js
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './WineMobile.css';
 
-import { useState, useRef } from "react";
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 import { useSpring, animated } from 'react-spring';
 import { connect } from 'react-redux';
 import { addSimpleItemToCart } from '../../actions/userCart';
@@ -13,20 +11,52 @@ import chard from '../../images/wine/chardonnay.png';
 
 
 function WineMobile(props) {
+
+
+
+  const [triggerCab, setCab] = useState(false);
+  const [hideChard, setHideChard] = useState(false);
+  const cabImgStyle = useSpring({
+    height: triggerCab ? '100%' : '145%',
+    left: triggerCab ? '37vw' : '7vw',
+    opacity: hideCab ? 0 : 1,
+  })
+
+  const [triggerChard, setChard] = useState(false);
+  const [hideCab, setHideCab] = useState(false);
+  const chardImgStyle = useSpring({
+    height: triggerCab ? '100%' : '145%',
+    right: triggerChard ? '37vw' : '7vw',
+    opacity: hideChard ? 0 : 1,
+  })
+
+  const handleCabClick = () => {
+    setCab(true);
+    setHideChard(true);
+  }
+  const handleChardClick = () => {
+    setChard(true);
+    setHideCab(true);
+  }
+
+  
+
   return (
     <div className="wine-mobile-cntr">
       
       <div className="wine-mobile-bottle-cntr">
-        <img
+        <animated.img
           src={cab}
           alt="We The People Cabernet Sauvignon"
-          className="wine-mobile-bottle-img"
+          className="wine-mobile-bottle-cab"
+          style={cabImgStyle}
         />
 
-        <img
+        <animated.img
           src={chard}
           alt="We The People Chardonnay"
-          className="wine-mobile-bottle-img"
+          className="wine-mobile-bottle-chard"
+          style={chardImgStyle}
         />
       </div>
 
@@ -35,6 +65,7 @@ function WineMobile(props) {
       <div className="wine-mobile-btn-cntr">
         <button
           className="wine-mobile-btn-cab"
+          onClick={handleCabClick}
         >
           CABERNET
         </button>
