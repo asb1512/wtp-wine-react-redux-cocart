4af7669b108b0c4c20dd8f7be432e96571854afd
asb1512 <andrew@olympicmedia.com>
2021-11-25 02:17:06 -0500

    building out animation with add-to-cart buttons

--- src/pages/wine/WineMobile.js
+++ src/pages/wine/WineMobile.js
@@ -14,29 +14,47 @@ function WineMobile(props) {
 
 
 
-  const [triggerCab, setCab] = useState(false);
+  const [triggerCab, setTriggerCab] = useState(false);
   const [hideChard, setHideChard] = useState(false);
+  const [triggerChard, setTriggerChard] = useState(false);
+  const [hideCab, setHideCab] = useState(false);
+  const [hideBottom, setHideBottom] = useState(false);
+
   const cabImgStyle = useSpring({
     height: triggerCab ? '100%' : '145%',
     left: triggerCab ? '37vw' : '7vw',
     opacity: hideCab ? 0 : 1,
   })
-
-  const [triggerChard, setChard] = useState(false);
-  const [hideCab, setHideCab] = useState(false);
   const chardImgStyle = useSpring({
-    height: triggerCab ? '100%' : '145%',
+    height: triggerChard ? '100%' : '145%',
     right: triggerChard ? '37vw' : '7vw',
     opacity: hideChard ? 0 : 1,
   })
+  const bottomStyle = useSpring({
+    opacity: hideBottom ? 0 : 1,
+  })
+  const cabBgStyle = useSpring({
+    height: triggerCab ? '90vh' : '0vh',
+  })
+  const chardBgStyle = useSpring({
+    height: triggerChard ? '100vh' : '0vh',
+  })
+  const cabTitleStyle = useSpring({
+    left: triggerCab ? '24vw' : '-100vw',
+  })
+  const chardTitleStyle = useSpring({
+    right: triggerChard ? '16vw' : '-100vw',
+  })
 
   const handleCabClick = () => {
-    setCab(true);
+    setTriggerCab(true);
     setHideChard(true);
+    setHideBottom(true);
   }
   const handleChardClick = () => {
-    setChard(true);
+    setTriggerChard(true);
     setHideCab(true);
+    setHideBottom(true);
   }
 
   
@@ -48,34 +66,70 @@ function WineMobile(props) {
         <animated.img
           src={cab}
           alt="We The People Cabernet Sauvignon"
-          className="wine-mobile-bottle-cab"
+          className="wine-mobile-bottle-img"
           style={cabImgStyle}
         />
 
         <animated.img
           src={chard}
           alt="We The People Chardonnay"
-          className="wine-mobile-bottle-chard"
+          className="wine-mobile-bottle-img"
           style={chardImgStyle}
         />
       </div>
 
-      <div className="wine-mobile-vertical-lines"/>
+
+      <animated.div 
+        className="wine-mobile-vertical-lines"
+        style={bottomStyle}
+      />
+
 
       <div className="wine-mobile-btn-cntr">
-        <button
+        <animated.button
           className="wine-mobile-btn-cab"
+          style={bottomStyle}
           onClick={handleCabClick}
         >
           CABERNET
-        </button>
+        </animated.button>
 
-        <button
+        <animated.button
           className="wine-mobile-btn-chard"
+          style={bottomStyle}
+          onClick={handleChardClick}
         >
           CHARDONNAY
-        </button>
+        </animated.button>
       </div>
+
+
+      {/* after-click state */}
+      <div className="wine-mobile-title-cntr">
+        <animated.div
+          className="wine-mobile-title"
+          style={cabTitleStyle}
+        >
+          CABERNET
+        </animated.div>
+
+        <animated.div
+          className="wine-mobile-title"
+          style={chardTitleStyle}
+        >
+          CHARDONNAY
+        </animated.div>
+      </div>
+
+      <animated.div
+        className="wine-mobile-cab-bg"
+        style={cabBgStyle}
+      />
+
+      <animated.div
+        className="wine-mobile-chard-bg"
+        style={chardBgStyle}
+      />
 
     </div>
   )
