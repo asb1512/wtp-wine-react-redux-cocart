42f55914bc18a5b51c761eb270d9e721013905f2
asb1512 <andrew@olympicmedia.com>
2022-01-14 15:20:04 -0500

    restyled order(s) show page

--- src/pages/dashboard/Orders.js
+++ src/pages/dashboard/Orders.js
@@ -3,40 +3,51 @@ import { connect } from 'react-redux';
 
 function Orders(props) {
 
+  const parseOrderDate = (date) => {
+    let splitDate = date.split('T')
+    return `${splitDate[0]} @ ${splitDate[1]}`
+  }
+
   if (props.orders) {
     return (
       <div className="dashboard-subcontainer">
-        <table className="orders-table">
-          <thead className="orders-table-header">
-            <tr className="orders-table-row">
-              <th>ORDER</th>
-              <th>DATE</th>
-              <th>STATUS</th>
-              <th>TOTAL</th>
-            </tr>
-          </thead>
-          <tbody>
+
+        <div className="orders-table">
+
+          <div className="orders-table-header">
+            <div>ORDER</div>
+            <div>DATE</div>
+            <div>STATUS</div>
+            <div>TOTAL</div>
+          </div>
+
+          <div>
             {props.orders?.map(order => {
-              console.log("single order id", order.id)
               return (
-                <tr key={order.id} className="orders-table-row">
-                  <td>{order.id}</td>
-                  <td>DATE</td>
-                  <td>{order.status}</td>
-                  <td>${order.total}<br />for {order.line_items.length} item(s)</td>
-                </tr>
+                <div key={order.id} className="orders-table-row">
+
+                  <div>{order.id}</div>
+                  <div>{parseOrderDate(order.date_created)}</div>
+                  <div>{order.status}</div>
+                  <div>${order.total}<br />for {order.line_items.length} item(s)</div>
+
+                </div>
               )
             })}
-          </tbody>
-        </table>
+          </div>
+
+        </div>
+
       </div>
     )
   } else {
     return (
       <div className="dashboard-subcontainer">
+        
         <div className="dashboard-no-content">
           You don't have any orders at this time.
         </div>
+
       </div>
     )
   }
